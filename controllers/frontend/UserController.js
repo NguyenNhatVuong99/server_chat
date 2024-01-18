e7b0bb5dfae4fa33fab4a5ce12b8025005ef6b4d
NguyenNhatVuong99 <nhatvuong0699@gmail.com>
2024-01-18 11:03:52 +0700

    ok

--- controllers/frontend/UserController.js
+++
@@ -1,8 +0,0 @@
-let User = require("../../models/User")
-let newfeed = (req, res) => {
-    res.render("layout/user/newfeed")
-}
-let messages = (req, res) => {
-    res.render("layout/user/messages")
-}
-module.exports = { newfeed, messages }
